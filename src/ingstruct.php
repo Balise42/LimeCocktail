96719059d27f1118c150437f9b9112fbc1bdeb10
Isabelle Hurbain-Palatin <isabelle@palatin.fr>
2023-04-04 22:56:14 +0200

    script to get ingredient structure

---
+++ src/ingstruct.php
@@ -0,0 +1,49 @@
+<html>
+<head>
+</head>
+<body>
+<ul>
+
+<?php
+
+require "../vendor/autoload.php";
+
+use CocktailSearch\DataStore;
+
+$store = DataStore::fromFlatFile( '../data/cocktails.txt' );
+
+$hierarchy = [];
+
+foreach ( $store->getStore() as $item ) {
+    foreach ( array_merge( $item->isInstanceOf, $item->isSubclassOf ) as $super ) {
+        if ( !array_key_exists( $super, $hierarchy) ) {
+            $hierarchy[$super] = [];
+        }
+        $hierarchy[$super][] = $item->name;
+    }
+}
+
+foreach ( $hierarchy as $k => $list ) {
+    sort( $list );
+    $hierarchy[$k] = array_unique( $list );
+
+}
+
+makeList( 'ingredient', $hierarchy );
+
+function makeList( string $ing, array $hierarchy ) {
+    echo "<li>$ing" . PHP_EOL;
+    if ( array_key_exists( $ing, $hierarchy) && count( $hierarchy[$ing] ) > 0 ) {
+        echo "<ul>" . PHP_EOL;
+        foreach ( $hierarchy[$ing] as $sub ) {
+            makeList( $sub, $hierarchy );
+        }
+        echo "</ul>" . PHP_EOL;
+    }
+    echo "</li>" . PHP_EOL;
+}
+
+?>
+</ul>
+</body>
+</html>
